6e8a84434a0af6986213065516dcf32a12574039
Romain Gaucher <neuroo@users.noreply.github.com>
2020-10-01 09:28:40 +0200

    Update research.js

--- app/routes/research.js
+++ app/routes/research.js
@@ -13,8 +13,8 @@ function ResearchHandler (db) {
             const url = `${req.query.url}/${symbol}`; 
             return needle.get(url, (error, newResponse) => {
                 if (!error && newResponse.statusCode == 200)
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.write(`<h1>The following is the stock ${prettyName(symbol)} information you requested.</h1>\n\n`);
+                    res.writeHead(200, {'Content-Type': 'text/plain'});
+                    res.write(`The following is the stock ${prettyName(symbol)} information you requested.\n\n`);
                     res.write('\n\n');
                     res.write(newResponse.body);
                     return res.end();
